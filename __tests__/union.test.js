4973f6485a1ae69a65c697503eee0c57f78b8b08
paqura <bloodking666@mail.ru>
2019-02-02 18:24:13 +0400

    Update union.test.js

--- __tests__/union.test.js
+++ __tests__/union.test.js
@@ -1,7 +1,7 @@
 import {union} from '../';
 
 describe('Union method test', () => {
-  it('should returns the correct flatten array without same item', () => {
+  it('should returns the correct flatten array without similar item', () => {
     const arr1 = [1, 2, 2, 3];
     const arr2 = [3, 4, 5];
     const expectResult = [1, 2, 3, 4, 5];
